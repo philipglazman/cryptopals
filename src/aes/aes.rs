271ea8f84a4e337534b150ba607130a1c3242338
Philip Glazman <8378656+philipglazman@users.noreply.github.com>
2021-12-29 13:31:07 -0800

    Challenge 11 solution.

--- src/aes/aes.rs
+++ src/aes/aes.rs
@@ -1,9 +1,58 @@
+use rand::prelude::ThreadRng;
+use rand::Rng;
+use std::io::Read;
 use std::vec;
 
 // challenge 7
 use crate::utils::XOR;
 use crate::utils::{self, Padding};
 use openssl::symm::{decrypt, encrypt, Cipher};
+
+#[derive(Debug, Clone, PartialEq)]
+enum Mode {
+    ECB,
+    CBC,
+}
+
+fn encryption_oracle(plaintext: &[u8]) -> Result<(Vec<u8>, Mode), Error> {
+    // append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext
+    let prepend_len = rand::thread_rng().gen_range(5..11);
+    let mut prepend: Vec<u8> = (0..prepend_len)
+        .map(|_| rand::thread_rng().gen::<u8>())
+        .collect();
+
+    let append_len = rand::thread_rng().gen_range(5..11);
+    let mut append: Vec<u8> = (0..append_len)
+        .map(|_| rand::thread_rng().gen::<u8>())
+        .collect();
+
+    let mut plaintext = plaintext.to_vec();
+    plaintext.splice(0..0, prepend.drain(..));
+    plaintext.append(&mut append);
+
+    let key = gen_16_byte();
+
+    // encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC)
+    let result = match rand::thread_rng().gen_bool(0.5) {
+        true => {
+            let iv = gen_16_byte();
+            let ciphertext = encrypt_aes_128_cbc(&plaintext, &key, &iv)?;
+            (ciphertext, Mode::CBC)
+        }
+        false => {
+            let ciphertext = encrypt_aes_128_ecb(&plaintext, &key)?;
+            (ciphertext, Mode::ECB)
+        }
+    };
+
+    Ok(result)
+}
+
+// Generates a random 16 byte vector.
+fn gen_16_byte() -> Vec<u8> {
+    let mut rng: ThreadRng = rand::thread_rng();
+    rng.gen::<[u8; 16]>().to_vec()
+}
 
 fn encrypt_aes_block(block: &[u8], key: &[u8]) -> Result<Vec<u8>, Error> {
     if block.len() != 16 {
@@ -99,6 +148,17 @@ fn encrypt_aes_128_cbc(plaintext: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8
     Ok(res.0)
 }
 
+fn detect_ecb(ciphertext: &[u8]) -> bool {
+    let mut map: std::collections::HashMap<&[u8], usize> = std::collections::HashMap::new();
+
+    for block in ciphertext.chunks(16) {
+        let count = map.entry(block).or_insert(0);
+        *count += 1;
+    }
+
+    map.iter().any(|(_block, count)| count > &1)
+}
+
 #[derive(Debug, Clone)]
 pub enum Error {
     InvalidIVLength,
@@ -139,7 +199,7 @@ mod tests {
     #[test]
     fn aes_cbc() {
         // Test AES-CBC
-        let plaintext = "ABCDEFGHIJKLMNOP".as_bytes();
+        let plaintext = "abcdefghijklmnop".as_bytes();
         let key = "YELLOW SUBMARINE".as_bytes();
         let iv = vec![0; 16];
         assert_eq!(
@@ -170,16 +230,7 @@ mod tests {
                 let line = line.unwrap();
                 let buf = utils::from_hex(&line).unwrap();
 
-                let mut map: std::collections::HashMap<&[u8], usize> =
-                    std::collections::HashMap::new();
-                for block in buf.chunks(16) {
-                    let count = map.entry(block).or_insert(0);
-                    *count += 1;
-                }
-
-                let dup = map.iter().any(|(_block, count)| count > &1);
-
-                if dup {
+                if detect_ecb(&buf) {
                     break Some(line);
                 }
             } else {
@@ -202,5 +253,21 @@ mod tests {
         let iv = vec!['\x00' as u8; 16];
         let plaintext = Vec::from("I\'m back and I\'m ringin\' the bell \nA rockin\' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that\'s my DJ Deshay cuttin\' all them Z\'s \nHittin\' hard and the girlies goin\' crazy \nVanilla\'s on the mike, man I\'m not lazy. \n\nI\'m lettin\' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse\'s to the side yellin\', Go Vanilla Go! \n\nSmooth \'cause that\'s the way I will be \nAnd if you don\'t give a damn, then \nWhy you starin\' at me \nSo get off \'cause I control the stage \nThere\'s no dissin\' allowed \nI\'m in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n\' play \n\nStage 2 -- Yea the one ya\' wanna listen to \nIt\'s off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI\'m an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI\'m like Samson -- Samson to Delilah \nThere\'s no denyin\', You can try to hang \nBut you\'ll keep tryin\' to get my style \nOver and over, practice makes perfect \nBut not if you\'re a loafer. \n\nYou\'ll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I\'m comin\' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin\' \nVanilla Ice is sellin\' and you people are buyin\' \n\'Cause why the freaks are jockin\' like Crazy Glue \nMovin\' and groovin\' trying to sing along \nAll through the ghetto groovin\' this here song \nNow you\'re amazed by the VIP posse. \n\nSteppin\' so hard like a German Nazi \nStartled by the bases hittin\' ground \nThere\'s no trippin\' on mine, I\'m just gettin\' down \nSparkamatic, I\'m hangin\' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n\'89 in my time! You, \'90 is my year. \n\nYou\'re weakenin\' fast, YO! and I can tell it \nYour body\'s gettin\' hot, so, so I can smell it \nSo don\'t be mad and don\'t be sad \n\'Cause the lyrics belong to ICE, You can call me Dad \nYou\'re pitchin\' a fit, so step back and endure \nLet the witch doctor, Ice, do the dance to cure \nSo come up close and don\'t be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that I was weak, Boy, you\'re dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you say it, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n");
         assert_eq!(plaintext, decrypt_aes_128_cbc(&buf, &key, &iv).unwrap());
+    }
+
+    #[test]
+    fn ecb_cbc_detection_oracle_challenge() {
+        // An ECB/CBC detection oracle
+        // https://cryptopals.com/sets/2/challenges/11
+        for _ in 0..10 {
+            // Plaintext must be of sufficient length in order to have at least multiple blocks.
+            // We can check for ECB encryption if two blocks are equal.
+            let plaintext = "abcdefghijklmnopabcdefghijklmnopabcdefghijklmnop".as_bytes();
+            let res = encryption_oracle(plaintext).unwrap();
+            match detect_ecb(&res.0) {
+                true => assert_eq!(Mode::ECB, res.1),
+                false => assert_eq!(Mode::CBC, res.1),
+            }
+        }
     }
 }
